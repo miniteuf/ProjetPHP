f00fda9974a46935522fb5a3e17485b9d0b6fae8
minideuf <remi.defaria@gmail.com>
2021-06-20 14:35:51 +0200

    test commit

---
+++ src/Controller/GameController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class GameController extends AbstractController
+{
+    #[Route('/game', name: 'game')]
+    public function index(): Response
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/GameController.php',
+        ]);
+    }
+}
